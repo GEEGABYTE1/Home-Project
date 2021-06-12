ea904eec0f3cb850e4416e9c8a85561c29f5ea27
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-06-11 21:26:13 -0400

    RGB Lighting With IR integration

--- Complete_Home.ino
+++ Complete_Home.ino
@@ -3,8 +3,12 @@
 int IRPIN = 2;
 IRrecv irrecv(IRPIN);
 decode_results results;
-
-
+int redpin = 6;
+int greenpin = 7;
+int bluepin = 5;
+int greenpin_count;
+int bluepin_count;
+int redpin_count;
 
 void setup() {
 Serial.begin(9600);
@@ -15,6 +19,9 @@ Serial.begin(9600);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   Serial.println("Enabled IRin");
+  pinMode(redpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
 
 }
 
@@ -61,7 +68,42 @@ if(irrecv.decode(&results))
       {
         digitalWrite(3, LOW);
       }
+
+    else if(results.value==16736925)
+      { 
+        
+        digitalWrite(redpin, HIGH);
+        digitalWrite(bluepin, LOW);
+        digitalWrite(greenpin, LOW);
+        redpin_count = 1;
+      }
+    else if(results.value==16761405)
+      {
+        
+        digitalWrite(greenpin, HIGH);
+        digitalWrite(bluepin, LOW);
+        digitalWrite(redpin, LOW);
+        greenpin_count = 1;
+      }
+     else if(results.value==16720605)
+      {
+        
+        digitalWrite(bluepin, HIGH);
+        digitalWrite(redpin, LOW);
+        digitalWrite(greenpin, LOW);
+        bluepin_count = 1;
+        Serial.println(bluepin_count);
+      }
+     else if(results.value==16754775)
+      {
+        digitalWrite(redpin, LOW);
+        digitalWrite(bluepin, LOW);
+        digitalWrite(greenpin, LOW);
+      }
+    
+     
     irrecv.resume();
   }
+  delay(500);
 
 }
